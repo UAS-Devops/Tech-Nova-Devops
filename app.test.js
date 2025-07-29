d77648ea42721b6f092caab8f46cb9ad1c7c8e11
Hasyim Adani <156512831+halvest@users.noreply.github.com>
2025-07-29 14:37:18 +0700

    KEY-15 Update app.test.js

--- app.test.js
+++ app.test.js
@@ -29,7 +29,7 @@ describe('Authentication Endpoints', () => {
 
         expect(response.statusCode).toBe(200);
         expect(response.body.success).toBe(true);
-        expect(response.body.message).toBe('Login berhasil! Mengarahkan...');
+        expect(response.body.message).toBe('Login berhasil! Segera Mengarahkan...');
         expect(response.headers['set-cookie']).toBeDefined();
     });
 
